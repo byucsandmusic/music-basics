23aabe4fb129b0e34f2b2a8f4bc87ac815cea4dc
Jacob Speakman <speakman610@gmail.com>
2025-01-24 11:42:48 -0700

    feat: create translator and base languages

---
+++ src/models/languages.ts
@@ -0,0 +1,25 @@
+export const languages = {
+  home: {
+    title: {
+      EN: 'Music App'
+    },
+  },
+  module1: {
+    title: {
+      EN: 'Basics'
+    },
+  },
+  module2: {
+    title: {
+      EN: 'Rhythmn'
+    },
+  },
+  module3: {
+    title: {
+      EN: 'SATB'
+    },
+  },
+  general: {
+
+  },
+}
---
+++ src/models/translator.ts
@@ -0,0 +1,21 @@
+import { languages } from './languages.ts'
+
+class Translator {
+  language:string = ''
+
+  constructor(language:string) {
+    this.language = language
+  }
+
+  /**
+   * @param path an array of strings pointing to the desired text in languages
+   * @returns the desired text in the current language
+   */
+  get(path:Array<string>) {
+    let temp = languages
+    for (const key of path) {
+      temp = temp[key]
+    }
+    return temp[this.language]
+  }
+}
