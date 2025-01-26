3e5a0f35c23cabe29b0cf48847e24dbce0270e9f
Jacob Speakman <speakman610@gmail.com>
2025-01-26 16:08:51 -0700

    test: added tests for the translator

--- src/models/translator.ts
+++ src/models/translator.ts
@@ -1,21 +1,36 @@
-import { languages } from './languages.ts'
+import { languages } from './languages'
 
 export default class Translator {
   language:string = ''
 
+  /**
+   * A translator that can retrieve text from the language.ts file based on the set language
+   * @param language a string representing the language to be used by the translator
+   */
   constructor(language:string) {
     this.language = language
   }
 
   /**
+   * Change the language used by the translator
+   * @param language a string representing the language to be used by the translator
+   */
+  setLanguage(language:string) {
+    this.language = language
+  }
+
+  /**
    * @param path an array of strings pointing to the desired text in languages
-   * @returns the desired text in the current language
+   * @returns the desired text in the current language,
+   * the desired text in English if the current language is invalid,
+   * or undefined if the path is incorrect
    */
   get(path:Array<string>) {
     let temp = languages
     for (const key of path) {
       temp = temp[key]
+      if (temp === undefined) return temp
     }
-    return temp[this.language]
+    return temp[this.language] || temp['EN']
   }
 }
---
+++ test/models/translator.test.ts
@@ -0,0 +1,32 @@
+// Set up mocks
+jest.mock("../../src/models/languages", () => ({
+  languages: {
+    home: {
+      title: {
+        EN: 'Mocked Home Title',
+      },
+    },
+  },
+}))
+
+// This must come after the mocks so that it uses our mocked languages file
+import Translator from '../../src/models/translator'
+
+describe('Translator Tests', () => {
+  test('Should retrieve text when a valid language is provided and the path is correct', () => {
+    const translator = new Translator('EN')
+    expect(translator.get(['home', 'title'])).toBe('Mocked Home Title')
+  })
+
+  test('Should retrieve text in EN when an invalid language is provided but the path is correct', () => {
+    const translator = new Translator('FR')
+    expect(translator.get(['home', 'title'])).toBe('Mocked Home Title')
+  })
+
+  test('Should return undefined when the path is incorrect regardless of language', () => {
+    const translator = new Translator('EN')
+    expect(translator.get(['some', 'path'])).toBeUndefined()
+    translator.setLanguage('FR')
+    expect(translator.get(['some', 'path'])).toBeUndefined()
+  })
+})
