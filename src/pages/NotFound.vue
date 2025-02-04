f5d2d30ea9a9d3d56aab708989ac40bc4387e0da
Megan Loertscher <pearl@loertscher.org>
2025-02-03 23:26:53 -0700

    feat: add a redirect page for bad urls

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,21 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Translator from '../models/translator'
+
+export default defineComponent({
+    name: 'NotFound',
+    props: {
+        translator: Translator,
+    },
+    mounted() {
+        setTimeout(() => this.$router.push('/home'), 1000)
+    },
+})
+</script>
+
+<template>
+    <h1>{{ translator.get('general', 'notFound', 'header') }}</h1>
+    <p>{{ translator.get('general', 'notFound', 'body') }}</p>
+</template>
+
+<style scoped></style>
