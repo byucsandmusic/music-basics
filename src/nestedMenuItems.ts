8fb2a333f6ee8c3f31cea74993d91b9d8c2f0d8d
suopuwu <Josephlu8@gmail.com>
2025-02-09 23:29:38 -0700

    Update nestedMenuItems.ts

--- src/nestedMenuItems.ts
+++ src/nestedMenuItems.ts
@@ -4,17 +4,14 @@ export default function useNestedMenuItems(translator: Translator) {
     return {
         demo: [
             {
-                id: 1,
                 href: '/demo/home',
                 title: translator.get('general', 'home'),
             },
             {
-                id: 2,
                 href: '/demo/interact',
                 title: translator.get('general', 'interactive', 'title'),
             },
             {
-                id: 3,
                 href: '/demo/music-notation',
                 title: translator.get('general', 'musicNotation', 'title'),
             },
