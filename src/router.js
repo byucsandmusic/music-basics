889341ebfed42fe302c173f6369793d7def3a1d0
Jacob Speakman <speakman610@gmail.com>
2025-02-10 20:05:41 -0700

    feat: remove extras from song in sing along

--- src/router.js
+++ src/router.js
@@ -36,22 +36,12 @@ const routes = [
                             'EGEG|GFDF|BBA3/A/|G3z|',
                             'EGEG|cAF3/D/|FFED|C3z|]',
                         ],
-                        // bass: [
-                        //     'E,E,/E,/F,G,|E,3G,|c,3/c,/B,A,|G,3G,|',
-                        //     'G,E,E,G,|G,F,F,3/E,/|E,D,B,A,|G,3z|',
-                        //     'E,G,E,G,|G,F,D,F,|B,B,A,3/A,/|G,3z|',
-                        //     'E,G,E,G,|c,A,F,3/D,/|F,F,E,D,|C,3z|]',
-                        // ],
                         verses: [
                             [
                                 'I am a child of God, And he has sent me here, Has',
                                 'giv-en me an earth-ly home With par-ents kind and dear.',
                                 'Lead me, guide me, walk be-side me, Help me find the way.',
                                 'Teach me all that I must do To live with him some-day.',
-                            ],
-                            [
-                                'I am a child of God, And so my needs are great; Help',
-                                'me to un-der-stand his words Be-fore it grows too late',
                             ],
                         ],
                     },
