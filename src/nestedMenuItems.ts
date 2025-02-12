d8fb432b021934ded55706c20ed9f9faf8f95c74
suopuwu <Josephlu8@gmail.com>
2025-02-11 18:30:01 -0700

    Revert "Update nestedMenuItems.ts"
    This reverts commit 611587b1e31088b7627bd7230ffcb1b9c086dc10.

--- src/nestedMenuItems.ts
+++ src/nestedMenuItems.ts
@@ -4,14 +4,17 @@ export default function useNestedMenuItems(translator: Translator) {
     return {
         demo: [
             {
+                id: 1,
                 href: '/demo/home',
                 title: translator.get('general', 'home'),
             },
             {
+                id: 2,
                 href: '/demo/interact',
                 title: translator.get('general', 'interactive', 'title'),
             },
             {
+                id: 3,
                 href: '/demo/music-notation',
                 title: translator.get('general', 'musicNotation', 'title'),
             },
