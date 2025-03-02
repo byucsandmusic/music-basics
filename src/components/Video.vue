1e16f7c0fdfc65d174ef34f2b3ce4cfad9e06fba
Megan Loertscher <pearl@loertscher.org>
2025-03-02 01:22:33 -0700

    fix: remove unused dependencies & fix typo
    - typo in PR workflow
    - unused imports in DragAndDropDemo.vue, Video.vue, and Sidebar.test.ts

--- src/components/Video.vue
+++ src/components/Video.vue
@@ -1,5 +1,5 @@
 <script lang="ts">
-import { computed, defineComponent } from 'vue'
+import { defineComponent } from 'vue'
 import Translator from '../models/translator'
 import { useRoute } from 'vue-router'
 
--- test/components/Exploration/Sidebar.test.ts
+++ test/components/Exploration/Sidebar.test.ts
@@ -1,5 +1,4 @@
 import { shallowMount } from '@vue/test-utils'
-import ClickableArea from '../../../src/components/Exploration/ClickableArea.vue'
 import Sidebar from '../../../src/components/Exploration/Sidebar.vue'
 import { expect } from 'vitest'
 
